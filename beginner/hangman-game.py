2c8791eba275614eefd512112b7945447f92c038
Wirtz Pendragon <adityawiranata2018@gmail.com>
2025-09-17 10:21:00 +0800

    add: hangman game

---
+++ beginner/hangman-game.py
@@ -0,0 +1,66 @@
+import random
+from collections import Counter
+
+someWords = '''apple banana orange mango grape peach cherry strawberry watermelon pineapple kiwi papaya apricot blueberry raspberry blackberry coconut fig guava lemon lime tangerine pomegranate cantaloupe honeydew nectarine persimmon starfruit dragonfruit durian jackfruit lychee rambutan passionfruit quince currant elderberry gooseberry huckleberry mulberry salak sapodilla soursop tamarind ugli fruit yuzu'''
+
+someWords = someWords.split()
+word = random.choice(someWords)
+
+if __name__ == '__main__':
+    print("Guess the word! HINT: it's a fruit")
+    
+    for i in word:
+        print('_', end=' ')
+    print()
+    
+    playing = True
+    letterGuessed = ''
+    chances = len(word) + 2
+    correct = 0
+    flag = 0
+    try:
+        while (chances != 0) and flag == 0:  
+            print()
+            chances -= 1
+            try:
+                guess = str(input('Enter a letter to guess: '))
+            except:
+                print('Enter only a letter!')
+                continue
+            if not guess.isalpha():
+                print('Enter only a LETTER')
+                continue
+            elif len(guess) > 1:
+                print('Enter only a SINGLE letter')
+                continue
+            elif guess in letterGuessed:
+                print('You have already guessed that letter')
+                continue
+            
+            if guess in word:
+                k = word.count(guess)
+                for _ in range(k):
+                    letterGuessed += guess
+            for char in word:
+                if char in letterGuessed and (Counter(letterGuessed) != Counter(word)):
+                    print(char, end=' ')
+                    correct += 1
+                elif (Counter(letterGuessed) == Counter(word)):
+                    print("The word is: ", end=' ')
+                    print(word)
+                    flag = 1
+                    print('Congratulations, You won!')
+                    break
+                else:
+                    print('_', end=' ')
+            
+        if chances <= 0 and (Counter(letterGuessed) != Counter(word)):
+            print()
+            print("You have run out of chances. The word was:", word)
+            print("Better luck next time!")
+            
+    except KeyboardInterrupt:
+        print()
+        print("Game interrupted. The word was:", word)
+        print("Better luck next time!")
+        exit()
